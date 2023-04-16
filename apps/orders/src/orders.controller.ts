859009a2a4c3376e745108333a31f082ceb57d6a
Pritam Nikam <pritam.nikam@gmail.com>
2023-04-16 21:29:55 +0530

    first commit
    Signed-off-by: Pritam Nikam <pritam.nikam@gmail.com>

---
+++ apps/orders/src/orders.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderRequest } from './dto/create-order-request';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  async createOrder(
+    @Body() request: CreateOrderRequest
+  ) {
+    return this.ordersService.createOrder(request);
+  }
+
+  @Get()
+  async getOrders() {
+    return this.ordersService.getOrders();
+  }
+
+  @Get(':id')
+  async getOrder(@Param('id') id: string) {
+    return this.ordersService.getOrderByID(id);
+  }
+
+}
